e6e38c144440d96f1bc3b9b9594e5c3c4dffbcba
Misha <damfik28@gmail.com>
2017-06-17 15:33:35 +0300

    calculator

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("img").slideToggle("slow");
-    });
+$(function() {
+  var myInput = $("#calculatorInput")[0];
+
+  $("button").click(function(event) {
+  	var myButton = event.target;
+    console.log("myButton", myButton.id, myButton.value);
+    myInput.value = myButton.value;
+  });
 });
